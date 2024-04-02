89f16a064ec21399f63cec08d22b72c24364a108
Sahil <sahil1372001@gmail.com>
2024-04-02 20:16:55 +0530

    Created new components and completed the home page

---
+++ src/Components/Assets/new_collections.js
@@ -0,0 +1,31 @@
+import p1_img from "./product_5.png";
+import p2_img from "./product_6.png";
+import p3_img from "./product_7.png";
+
+let new_collections = [
+    {
+      id: 5,
+      name: "SimpliClean Silchar - R22, Made with Mizoram Grass, Binded With Wire, Control Grip Accupressure Handle",
+      image: p1_img,
+      new_price: 260.0,
+      old_price: 410.5,
+    },
+    {
+      id: 6,
+      name: "SimpliClean Swirly's 5D, Shilong Grass, 1.25'' Chrome Plated MS Pipe Handle, Weight 450 Gms",
+      image: p2_img,
+      new_price: 169.0,
+      old_price: 250.0,
+    },
+    {
+      id: 7,
+      name: "SimpliClean RB-44, Non- Dust Broom, Long Handle, Washable and Effortless Cleaning, Weight 280 Gms",
+      image: p3_img,
+      new_price: 189.0,
+      old_price: 249.0,
+
+    }
+]
+
+export default new_collections;
+
